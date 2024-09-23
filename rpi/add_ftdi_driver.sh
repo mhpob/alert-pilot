dce433c1e0fa976a465375942d563e0cfe8d4f18
Mike O'Brien <obrien@umces.edu>
2024-09-23 18:11:26 -0400

    change to use arguments

--- rpi/add_ftdi_driver.sh
+++ rpi/add_ftdi_driver.sh
@@ -1,7 +1,8 @@
-echo 0c52 a021 > /sys/bus/usb-serial/drivers/ftdi_sio/new_id
+#!/bin/bash
+echo $1 $2 > /sys/bus/usb-serial/drivers/ftdi_sio/new_id
 
 # https://superuser.com/questions/685471/how-can-i-run-a-command-after-boot
 # change permissions (chmod 755 add_seacom_driver.sh)
 # switch to root (sudo su)
 # open crontab (crontab -e)
-# add reboot command (@reboot /home/secor/alert/add_seacom_driver.sh)
+# add reboot command (@reboot /home/secor/alert/add_seacom_driver.sh 0c52 a021)
