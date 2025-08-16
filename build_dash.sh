52419567c289f8f50aa9660e6e27103fcc72c82e
Mike O'Brien <obrien@umces.edu>
2025-08-15 20:25:03 -0400

    send slack alert

--- build_dash.sh
+++ build_dash.sh
@@ -2,4 +2,14 @@
 # Copy the files created by Docker in ./html (root) to ./_html (obrien)
 
 docker compose -f /users/obrien/alert-pilot/dashboard/docker-compose.yaml up
-cp -r /users/obrien/alert-pilot/dashboard/html/. /users/obrien/alert-pilot/dashboard/_html
+
+cp -rf /users/obrien/alert-pilot/dashboard/html/. /users/obrien/alert-pilot/dashboard/_html
+
+dc_now=$(tail -n 1 result/db.csv | sed -n 's/.*DC=\(.*\),PC.*/\1/p')
+dc_then=$(tail -n 2 result/db.csv | head -n 1 | sed -n 's/.*DC=\(.*\),PC.*/\1/p')
+
+if [ "$dc_now" -gt "$dc_then" ]; then
+  curl -X POST -H 'Content-type: application/json' \
+  --data '{"text":"Sturgeon detected! Current count: '"$dc_now"'"}' \
+  https://hooks.slack.com/services/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
+fi
